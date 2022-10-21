0a4e4af5c857e1ccc1ae0c7a36cbb3322f75e286
Joseph Foley <josephfoley@rcn.com>
2022-10-21 12:45:21 +0530

    Update restaurantapp.py

--- restaurantapp.py
+++ restaurantapp.py
@@ -5,11 +5,28 @@ import random
 import requests
 from tkinter.ttk import *
 
+
+
+
 ##root for Tkinter app
 root = tkinter.Tk()
 root.title("Tkinterpractice")
-root.geometry("600x400")
+root.geometry("800x400")
+with open('inputfile.txt', 'w') as fp:
+    fp.truncate(0)
+###takes input to add to inputfile
+def inputfile():
+   
+    locvarentry = input('Enter location ')
+    catvarentry = input('Enter cuisine ')
+    with open('inputfile.txt', 'w') as fw:
+        fw.write(str(locvarentry)+ ",")
+        fw.write(str(catvarentry)+ ",")
 
+
+    
+
+inputfile()
 ###deletes existing text from file
 with open('varfile.txt', 'w') as fp:
     fp.truncate(0)
@@ -18,24 +35,38 @@ API_test.API_call()
 ###pritns the text from the file after API call
 with open('varfile.txt', 'r') as newfile:
     var1 = newfile.readlines()
-print(var1)
+
 
 ##SPLITS var1 result into 3 different variables
 var1split = (var1[0].split(','))
 
+
+###Takes location
+
+location = (var1split[0])
+
+
+###Takes cuisine
+
+cuisine=(var1split[1])
+
 ###Takes number of restaurants from var1
-print(var1split[2])
+numberofrestaurants = (var1split[2])
 
 ##Genarates tkinter labels
-def size_2():
-   text.config(font=('Helvetica bold',40))
+
 
 ##Tkinter labels
 
-labeltitle = tkinter.Label(root, text="Restaurant app").pack()
-label  = tkinter.Label(root, text=var1).pack()
+labeltitle = tkinter.Label(root, text="Restaurant app",font=("Helvetica",40)).pack()
+label  = tkinter.Label(root, text="The number of "+ cuisine +  " restaurants in " + location+ " is " +numberofrestaurants,
+font=("Arial",24)).pack()
+
+
 
 ###mainloop for Tkinter window
 root.mainloop()
+
+
 
 
